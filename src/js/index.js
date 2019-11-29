d4da84bc97a9b3cde1006df1023cd31ef3e42517
natkalia <natalia.banasiewicz@wp.pl>
2019-11-29 14:46:41 +0100

    Update fetching data based on ibu,abv to return array

--- src/js/index.js
+++ src/js/index.js
@@ -183,37 +183,29 @@ rangeSliderIbuBar.noUiSlider.on('update', (values) => {
     getUserIbuInputs(values);
 });
 
-// this fetch is needed in searching by abv
-
-//  choosenMinAbvValue - choosen min abv value, choosenMaxAbvValue - choosen max abv value
-
+// ABV search - create function fetching data and returning array with data 
 function fetchBeerByAbv(choosenMinAbvValue, choosenMaxAbvValue){
     fetch(`https://api.punkapi.com/v2/beers?abv_gt=${choosenMinAbvValue}&abv_lt=${choosenMaxAbvValue}`)
     .then(response => {
         return response.json();
     })
     .then(data => { 
-        console.log(data);
-        //add here what we want to do with data
+        setResults(data);
     }).catch( error => {
         console.log('Błąd!', error);
     });
 };
 
-// this fetch is needed in searching by ibu 
-
-//  choosenMinIbuValue - choosen min ibu value, choosenMaxIbuValue - choosen max ibu value
-
+// IBU search - create function fetching data and returning array with data 
 function fetchBeerByIbu(choosenMinIbuValue, choosenMaxIbuValue){
     fetch(`https://api.punkapi.com/v2/beers?ibu_gt=${choosenMinIbuValue}&ibu_lt=${choosenMaxIbuValue}`)
     .then(response => {
         return response.json();
     })
     .then(data => { 
-        console.log(data);
-        //add here what we want to do with data
+        setResults(data);
     }).catch( error => {
-        console.log('Błąd!', error);
+        console.log('Error!', error);
     });
 };
 
