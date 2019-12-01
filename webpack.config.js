d5f0919b7f51dc23db9894a6cd5083c87d3fb7c7
anna <annapiwonska1@gmail.com>
2019-12-01 12:06:58 +0100

    Add dependencies and configuration (jquery, slick-carousel, @babel-polyfill)

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,59 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
 
-const distDir = path.resolve(__dirname, 'dist');
+const distDir = path.resolve(__dirname, "dist");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ["@babel/polyfill", "./src/js/index.js"],
   output: {
-    filename: 'index.js',
+    filename: "index.js",
     path: distDir
   },
-  plugins: [new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: 'src/index.html'
-  })],
-  mode: 'development',
-  devtool: 'eval',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html"
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })
+  ],
+  mode: "development",
+  devtool: "eval",
   devServer: {
     contentBase: distDir
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
-    }
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]"
+          }
+        }
+      }
     ]
-  },
-  
+  }
 };
